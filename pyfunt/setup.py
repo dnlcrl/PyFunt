6feacba6d74ab991d3bdf34c8430ec99e76bf538
Daniele Ciriello <imac@iMac.local>
2016-04-19 12:27:20 +0200

    adds more files from scipy

---
+++ pyfunt/setup.py
@@ -0,0 +1,13 @@
+from __future__ import division, print_function, absolute_import
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('pyfunt', parent_package, top_path)
+    config.add_subpackage('layers')
+    config.make_config_py()
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
