e322f15997b5fe5d1821db5691a4d20f08f1327b
dnlcrl <ciriello.daniele@gmail.com>
2016-10-08 21:57:20 +0200

    adds custom class loader

--- pyfunt/utils/__init__.py
+++ pyfunt/utils/__init__.py
@@ -1,2 +1,2 @@
-from load_torch_model import (load_t7model, load_parser_init, load_parser_vals)
+from load_torch_model import (load_t7model, load_t7checkpoint, load_parser_init, load_parser_vals)
 from . import *
