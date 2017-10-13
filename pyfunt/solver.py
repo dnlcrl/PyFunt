3a10eb9cd733489736f9a5517eae919e4c513ab6
Daniele Ciriello <dnlcrl@users.noreply.github.com>
2017-10-13 16:28:41 +0200

    Update solver.py

--- pyfunt/solver.py
+++ pyfunt/solver.py
@@ -18,36 +18,6 @@ def rel_error(x, y):
     """ returns relative error """
     return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
-# def _pickle_method(method):
-#     """
-#     Author: Steven Bethard (author of argparse)
-#     http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
-#     """
-#     func_name = method.im_func.__name__
-#     obj = method.im_self
-#     cls = method.im_class
-#     cls_name = ''
-#     if func_name.startswith('__') and not func_name.endswith('__'):
-#         cls_name = cls.__name__.lstrip('_')
-#     if cls_name:
-#         func_name = '_' + cls_name + func_name
-#     return _unpickle_method, (func_name, obj, cls)
-
-
-# def _unpickle_method(func_name, obj, cls):
-#     """
-#     Author: Steven Bethard
-#     http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
-#     """
-#     for cls in cls.mro():
-#         try:
-#             func = cls.__dict__[func_name]
-#         except KeyError:
-#             pass
-#         else:
-#             break
-#     return func.__get__(obj, cls)
-
 
 def _pickle_method(method):
     '''
